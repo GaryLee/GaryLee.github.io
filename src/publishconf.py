d19aa7054086222a54a8ba8d3e07ad3e472aeda3
garylee <garywlee@gmail.com>
2020-11-16 15:55:38 +0800

    - Upgraded pelican to 4.5.1. - Change theme to `nest`.

--- src/publishconf.py
+++ src/publishconf.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
 
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
@@ -10,15 +9,16 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
+# If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'http://garylee.github.io'
-RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
 
 DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "garyatgithub"
-GOOGLE_ANALYTICS = "UA-2953940-2"
+#DISQUS_SITENAME = ""
+#GOOGLE_ANALYTICS = ""
